4be65e45675c3c0031e0fac4e250b9f031d03d17
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2025-04-09 19:08:09 +0530

    chore: adjust deployment settings

--- src/datasource.ts
+++ src/datasource.ts
@@ -4,7 +4,7 @@ import { User } from "./entities/userModel";
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
-  port: 3306,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
